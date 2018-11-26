85afe2143817ee70e0fb13db2537ebc0015c54d7
Harutyun Khangeldyan <khangeldyanh@gmail.com>
2018-11-26 20:35:32 +0400

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device_width, user-scalable=no, initial-scale=1.0"
+		<title></title>
+	</head>
+
+	<body>
+		@yield('content')
+	</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'PostsController@index');
+Route::get('/tasks', 'TasksController@index');
+//Route::get('/tasks/{task}', 'TasksController@show');
+//Route::get('/tasks', function () {
+	//$tasks = DB::table('tasks')->get();
+	//$tasks = App\Task::all();
+//	$tasks = App\Task::incomplete();
+//    return view('tasks.index', compact('tasks'));
+//});
+
+//Route::get('/tasks/{task}', function ($id) {
+	//$task = DB::table('tasks')->find($id);
+//	$task = App\Task::find($id);
+//    return view('tasks.show', compact('task'));
+//});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
